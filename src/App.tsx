6cdac3ea8293234dcff53b110e5afe9ae0cb6be1
Kuzmrom7 <kuzmrom7@gmail.com>
2019-07-09 09:06:43 +0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,38 @@ import React, { useEffect, useState, useCallback } from "react";
 import useStoreon from "storeon/react";
 import FilmView from "./components/FilmView";
 
+import "./style.scss";
+
 interface IProps {}
 
 const App: React.FC<IProps> = props => {
   const { dispatch, film } = useStoreon("film");
   const [query, setQuery] = useState("");
 
-  const handleClick = useCallback(() => {
-    dispatch("film/search", query);
-  }, [query]);
+  const handleClick = useCallback(
+    e => {
+      e.preventDefault();
+      dispatch("film/search", query);
+    },
+    [query]
+  );
 
   return (
-    <div>
-      <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
-      <button onClick={handleClick}>Search</button>
+    <div className="wrap">
+      Search film:
+      <form onSubmit={handleClick}>
+        <div className="container">
+          <input
+            className="input-material"
+            type="text"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
+          <button className="btn" onClick={handleClick}>
+            Search
+          </button>
+        </div>
+      </form>
       <FilmView />
     </div>
   );
--- src/components/FilmView.tsx
+++ src/components/FilmView.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react";
 import useStoreon from "storeon/react";
 
+import "./style.scss";
+
 interface IProps {}
 
 const FilmView: FC<IProps> = props => {
@@ -8,9 +10,19 @@ const FilmView: FC<IProps> = props => {
   return (
     <>
       {Object.keys(film).length > 0 ? (
-        <div>
+        <div className="wrap-film">
           <h2> {film.Title}</h2>
           <img src={film.Poster} alt="Poster" />
+
+          <div className="info">
+            <h3>Actors</h3> {film.Actors}
+            <h3>Genre</h3> {film.Genre}
+            <h3>Awards</h3> {film.Awards}
+            <h3>Country </h3>
+            {film.Country}
+            <h3>Rating </h3>
+            {film.imdbRating}
+          </div>
         </div>
       ) : (
         ""
